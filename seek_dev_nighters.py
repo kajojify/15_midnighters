86553a9ada39b9da2a428739bf87a6830e8dfab8
Miroslav Vinogradov <rsamoilenko@mail.ru>
2016-10-07 18:22:18 +0300

    Коррективы в соответствии с графой "что не так"
    Удалён один лишний запрос к API.
    Внедрена корректная работа с временными зонами.
    Все магические числа вынесены в переменные с говорящими названиями.
    Добавлена обработка случая, когда API в качестве значения timestamp возвращает "null". 
    Удалены лишние комментарии.

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -1,42 +1,36 @@
 import requests
+from pytz import timezone
 from datetime import datetime, time
 
 
 def load_attempts():
-    """Загружает информацию с devman о том, кто и когда отправил задачу
-    на проверку, в виде json. Возвращает объект-генератор, на каждой итерации
-    генерирующий словарь, содержащий ник отправившего задачу, его временную
-    зону и количество секунд прошедшее с 00:00:00 UTC 1 января, 1970 года."""
-
     url = "https://devman.org/api/challenges/solution_attempts/"
-    devman_response = requests.get(url, params={'page': 1})
-    devman_data = devman_response.json()
-    pages = devman_data['number_of_pages']
-    for page_number in range(1, pages+1):
+    first_page = 1
+    last_page = 10
+    for page_number in range(first_page, last_page+1):
         payload = {'page': page_number}
         devman_response = requests.get(url, params=payload)
         devman_data = devman_response.json()
-        users = len(devman_data['records'])
-        for user_number in range(0, users):
+        users = devman_data['records']
+        for user in users:
             yield {
-                'username': devman_data['records'][user_number]['username'],
-                'timestamp': devman_data['records'][user_number]['timestamp'],
-                'timezone': devman_data['records'][user_number]['timezone'],
+                'username': user['username'],
+                'timestamp': user['timestamp'],
+                'timezone': user['timezone'],
             }
 
 
 def get_midnighters(users):
-    """Среди пользователей devman отбирает тех, кто отправлял задачи в
-    промежуток от 00:00 до 04:00. Возвращает список, элементами которого
-     являются ники "пользователей-сов". """
-
     midnight_users_list = []
     for user in users:
-        user_datetime = datetime.fromtimestamp(user['timestamp'])
-        # user_datetime содержит время уже относительно часовой зоны юзера,
-        # так как timestamp имеет значение относительно часовой зоны юзера
-        user_time = user_datetime.time()
-        if time(0, 0, 0) < user_time < time(4, 0, 0):
+        if user['timestamp'] is None:
+            continue
+        server_time_zone = timezone("Europe/Moscow")
+        server_datetime = datetime.fromtimestamp(user['timestamp'])
+        server_datetime_localized = server_time_zone.localize(server_datetime)
+        user_time_zone = timezone(user['timezone'])
+        user_time = server_datetime_localized.astimezone(user_time_zone)
+        if time(0, 0, 0) < user_time.time() < time(4, 0, 0):
             if user['username'] not in midnight_users_list:
                 midnight_users_list.append(user['username'])
     return midnight_users_list
@@ -47,3 +41,4 @@ if __name__ == '__main__':
     print("Совы devman, отправялющие задачи в промежуток 00:00 - 04:00:\n")
     for user in midnight_users_list:
         print(user)
+        
