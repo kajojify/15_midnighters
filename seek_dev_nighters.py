6435b99c12b3066e97f4d8a3bba79e4999dbe218
Miroslav Vinogradov <rsamoilenko@mail.ru>
2016-10-10 11:37:53 +0300

    Update seek_dev_nighters.py

--- seek_dev_nighters.py
+++ seek_dev_nighters.py
@@ -9,18 +9,26 @@ def load_attempts():
     devman_response = requests.get(url, params={'page': first_page})
     devman_data = devman_response.json()
     pages = devman_data['number_of_pages']
+    users = devman_data['records']
+    for user in users:
+            users_list.append({
+                'username': user['username'],
+                'timestamp': user['timestamp'],
+                'timezone': user['timezone'],
+            })
     for page_number in range(first_page + 1, pages + 1):
+        users_list = []
         payload = {'page': page_number}
         devman_response = requests.get(url, params=payload)
         devman_data = devman_response.json()
         users = devman_data['records']
         for user in users:
-            yield {
+            users_list.append({
                 'username': user['username'],
                 'timestamp': user['timestamp'],
                 'timezone': user['timezone'],
-            }
-
+            })
+        return users_list
 
 def get_midnighters(users):
     midnight_users_list = []
@@ -39,7 +47,7 @@ def get_midnighters(users):
 
 
 if __name__ == '__main__':
-    users_list = list(load_attempts())
+    users_list = load_attempts()
     midnight_users_list = get_midnighters(users_list)
     print("Совы devman, отправялющие задачи в промежуток 00:00 - 04:00:\n")
     for user in midnight_users_list:
